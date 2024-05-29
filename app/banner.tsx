9bd4e8b082e9ceabcf078629390833cba3d2c536
KPS <sachinkps1@gmail.com>
2024-05-29 07:12:21 +0400

    initial config

--- app/banner.tsx
+++ app/banner.tsx
@@ -4,18 +4,14 @@ import React from "react";
 
 function Banner() {
   return (
-    <section className="w-full h-[676px] pl-[125px] pt-[175px] bg-lime-200">
-      <h1 className="w-[579px] text-gray-800 text-base font-semibold font-['Montserrat'] tracking-wide">
-        TRANSPORTATION SOLUTIONS
-      </h1>
-      <div className="w-[579px]">
-        <h2 className="text-gray-800 text-[76px] font-semibold font-['Figtree'] leading-[80px]">
-          Sustainable logistics for a{" "}
-        </h2>
-        <h3 className="text-lime-700 text-[76px] font-semibold font-['Figtree'] leading-[80px]">
-          greener future
-        </h3>
-        <div className="mt-[32px]">
+    <section className="w-full h-svh bg-theme-200">
+      <div className="w-full max-w-320 h-full p-4 md:p-5 m-auto flex">
+        <div className="flex flex-col justify-center">
+          <span className="block">TRANSPORTATION SOLUTIONS</span>
+          <h1 className="text-6xl">
+            Sustainable logistics for a
+            <span className="block text-theme-text-700">greener future</span>
+          </h1>
           <Button
             type={"submit"}
             title={"Let's Talk"}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,55 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'theme-100': '#EEF5E1',
+      'theme-200': '#D5E5B8',
+      'theme-300': '#D9E27E',
+      'theme-400': '#9AB23E',
+      'theme-500': '#46731E',
+      'theme-600': '#225219',
+      'theme-text-300': '#ADD356',
+      'theme-text-700': '#4B8200',
+      'theme-text-900': '#0A1D06',
+    },
+    fontFamily: {
+      sans: ['Figtree', 'sans-serif'],
+      serif: ['Mate', 'serif'],
+    },
+    fontSize: {
+      'xl': ['1.25rem', {
+        lineHeight: '1.5',
+      }],
+      '2xl': ['1.5rem', {
+        lineHeight: '1.66',
+      }],
+      '3xl': ['1.75rem', {
+        lineHeight: '1.14rem',
+        fontWeight: '600',
+      }],
+      '4xl': ['2.25rem', {
+        lineHeight: '1.2',
+        fontWeight: '600',
+      }],
+      '5xl': ['3.25rem', {
+        lineHeight: '1.2',
+        fontWeight: '600',
+      }],
+      '6xl': ['4.75rem', {
+        lineHeight: '1.05',
+        fontWeight: '600',
+        letterSpacing: '-.01em'
+      }],
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      width: {
+        '320': '80rem'
+      }
     },
   },
   plugins: [],

8e18d0d1f59f4f99bdc17bda6a205440f8cdfdab
KPS <sachinkps1@gmail.com>
2024-06-11 12:47:43 +0400

    theme vars moved to extend

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,51 +7,51 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      'theme-100': '#EEF5E1',
-      'theme-200': '#D5E5B8',
-      'theme-300': '#D9E27E',
-      'theme-400': '#9AB23E',
-      'theme-500': '#46731E',
-      'theme-600': '#225219',
-      'theme-900': '#215218',
-      'theme-text-300': '#ADD356',
-      'theme-text-700': '#4B8200',
-      'theme-text-900': '#0A1D06',
-    },
     fontFamily: {
       sans: ['Figtree', 'sans-serif'],
       serif: ['Mate', 'serif'],
     },
-    fontSize: {
-      'xl': ['1.25rem', {
-        lineHeight: '1.5',
-      }],
-      '2xl': ['1.5rem', {
-        lineHeight: '1.66',
-      }],
-      '3xl': ['1.75rem', {
-        lineHeight: '1.14rem',
-        fontWeight: '600',
-      }],
-      '4xl': ['2.25rem', {
-        lineHeight: '1.2',
-        fontWeight: '600',
-      }],
-      '5xl': ['3.25rem', {
-        lineHeight: '1.2',
-        fontWeight: '600',
-      }],
-      '6xl': ['4.75rem', {
-        lineHeight: '1.05',
-        fontWeight: '600',
-        letterSpacing: '-.01em'
-      }],
-    },
     extend: {
       width: {
         '320': '80rem'
-      }
+      },
+      colors: {
+        'theme-100': '#EEF5E1',
+        'theme-200': '#D5E5B8',
+        'theme-300': '#D9E27E',
+        'theme-400': '#9AB23E',
+        'theme-500': '#46731E',
+        'theme-600': '#225219',
+        'theme-900': '#215218',
+        'theme-text-300': '#ADD356',
+        'theme-text-700': '#4B8200',
+        'theme-text-900': '#0A1D06',
+      },
+      fontSize: {
+        'xl': ['1.25rem', {
+          lineHeight: '1.5',
+        }],
+        '2xl': ['1.5rem', {
+          lineHeight: '1.66',
+        }],
+        '3xl': ['1.75rem', {
+          lineHeight: '1.14rem',
+          fontWeight: '600',
+        }],
+        '4xl': ['2.25rem', {
+          lineHeight: '1.2',
+          fontWeight: '600',
+        }],
+        '5xl': ['3.25rem', {
+          lineHeight: '1.2',
+          fontWeight: '600',
+        }],
+        '6xl': ['4.75rem', {
+          lineHeight: '1.05',
+          fontWeight: '600',
+          letterSpacing: '-.01em'
+        }],
+      },
     },
   },
   plugins: [],

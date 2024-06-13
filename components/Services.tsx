9163c6f8e2ceaae0c1791b438009cc4cb27564ff
jedinrk <jedi.nrk@gmail.com>
2024-06-13 12:19:03 +0400

    updated services component styles

--- components/Services.tsx
+++ components/Services.tsx
@@ -3,13 +3,13 @@ import Button from "./Button";
 
 function Services() {
   return (
-    <section className="py-[120px] bg-orange-100">
-      <div className="max-w-320 m-auto space-y-14">
+    <section className="w-full px-[124px] py-[120px] bg-theme-700">
+      <div className="flex flex-col max-w-320 m-auto space-y-14 items-center">
         <div>
           <h1 className="uppercase mb-[16px]">services</h1>
-          <div className="flex justify-between">
-            <h2>Green, efficient & reliable logistics</h2>
-            <p>
+          <div className="flex justify-between gap-[140px]">
+            <h2 className="text-5xl mr-[40px]">Green, efficient & reliable logistics</h2>
+            <p className="ml-[40px]">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               Suspendisse varius enim in eros elementum tristique. Duis cursus,
               mi quis viverra ornare, eros dolor interdum nulla, ut commodo diam

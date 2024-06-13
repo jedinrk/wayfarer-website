dfdd134a85b29a54fc1cbb930e396237da09728b
jedinrk <jedi.nrk@gmail.com>
2024-06-13 12:18:32 +0400

    added content and styling to CustomerTestimonial component

---
+++ components/CustomerTestimonials.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Testimonial, { Testimony } from "./Testimonial";
+
+const CustomerTestimonials: React.FC<{
+  testimonies: Testimony[];
+}> = ({ testimonies }) => {
+  return (
+    <>
+      <section className="px-[124px] py-[120px]">
+        <div>
+          <h1 className="uppercase">Customer testimonials</h1>
+          <h2 className="text-5xl w-1/2 mt-[24px]">Client Stories: <br/> Our Impact in Action</h2>
+          <div className="mt-[80px] flex justify-between gap-[64px]">
+            {testimonies.map((testimony, index) => (
+              <Testimonial key={index} testimony={testimony} />
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default CustomerTestimonials;
--- components/Testimonial.tsx
+++ components/Testimonial.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React from "react";
+import Rating from "./Rating";
+import Image from "next/image";
 
-function Testimonial() {
+export type Testimony = {
+  id: number;
+  name: string;
+  description: string;
+  profilePic: string;
+  company: string;
+  logo: string;
+  content: string;
+  rating: number;
+};
+
+const Testimonial: React.FC<{ testimony: Testimony }> = ({ testimony }) => {
+  // Destructure props for easier access
+  const { name, description, profilePic, company, logo, content, rating } =
+    testimony;
+
   return (
-    <section>Testimonial</section>
-  )
-}
+    <div className="testimonial" id={`${testimony.id} testimony`}>
+      <Rating rating={rating} />
+      <p className="text-gray-800 text-xl py-[32px] font-serif italic">
+        &quot;{content}&quot;
+      </p>
+      <div className="flex items-center">
+        <div className="flex border-r pr-20 gap-5">
+          {profilePic && (
+            <Image
+              src={profilePic}
+              alt={`${name}'s profile picture`}
+              width={56}
+              height={56}
+            />
+          )}
+          <div className="flex flex-col">
+            <h5>{name}</h5>
+            <h6>{description}</h6>
+          </div>
+        </div>
+        <div className="px-[20px]">
+          {logo && (
+            <Image src={logo} alt={`${company} logo`} width={152} height={36} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Testimonial
+export default Testimonial;

7742097507c9c3525159faebcd9afb1a3a9c7574
jedinrk <jedi.nrk@gmail.com>
2024-06-13 14:01:05 +0400

    Fixed MIssion padding

--- components/Mission.tsx
+++ components/Mission.tsx
@@ -2,18 +2,19 @@ import React from "react";
 
 function Mission() {
   return (
-    <section className="w-full pl-[124px] pr-[419px] py-[120px] bg-theme-900">
-      <div className="flex justify-between gap-[166px]">
-        <h1 className="opacity-70 text-[#FFFFFF] uppercase whitespace-nowrap">
-          Our Mission
-        </h1>
-        <p className="font-sans text-[#FFFFFF] text-2xl leading-10">
-          We're on a mission to revolutionise container logistics through
-          sustainable practices that prioritise green energy, power efficiency,
-          and minimal environmental impact.
-          <br/>
-          <span className="text-xl text-[#ADD356]">Know More...</span>
-        </p>
+    <section className="w-full bg-theme-900">
+      <div className="w-full max-w-320 m-auto">
+        <div className="flex gap-[166px] py-[120px]">
+          <h1 className="opacity-70 text-[#FFFFFF] uppercase whitespace-nowrap">
+            Our Mission
+          </h1>
+          <p className="max-w-[600px] font-sans text-[#FFFFFF] text-2xl leading-10">
+            We're on a mission to revolutionise container logistics through
+            sustainable practices that prioritise green energy, power
+            efficiency, and minimal environmental impact.
+            <span className="text-xl text-[#ADD356]"> Know More...</span>
+          </p>
+        </div>
       </div>
     </section>
   );

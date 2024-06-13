dedbf37be3fb058d92f54a6e1f94fd5f17281fb7
jedinrk <jedi.nrk@gmail.com>
2024-06-13 12:17:20 +0400

    added remote url pattern in next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        //https://cdn.prod.website-files.com/6365d860c7b7a7191055eb8a/66404ae94fe39eba6eb74858_AlphaWave.svg
+        protocol: 'https',
+        hostname: 'cdn.prod.website-files.com',
+        port: '',
+        pathname: '/**/**',
+      },
+      {
+       //https://avatar.iran.liara.run/public/34
+        protocol: 'https',
+        hostname: 'avatar.iran.liara.run',
+        port: '',
+        pathname: '/public/**',
+      },
+    ],
+  },
+};
 
 export default nextConfig;

65bbc93d371d05008f478c6ed91f9f82a56a5143
jedinrk <jedi.nrk@gmail.com>
2024-06-21 03:29:42 +0400

    fixing casing for letters in Navbar

--- constants/index.ts
+++ constants/index.ts
@@ -2,7 +2,7 @@
 export const NAV_LINKS = [
   { href: '#mission', key: 'mission', label: 'Mission' },
   { href: '#services', key: 'services', label: 'Services' },
-  { href: '#about-us', key: 'about_us ', label: 'About us ' },
-  { href: '#contact-us', key: 'contact_us', label: 'Contact us' },
+  { href: '#about-us', key: 'about_us ', label: 'About Us ' },
+  { href: '#contact-us', key: 'contact_us', label: 'Contact Us' },
   // { href: '#community', key: 'community', label: 'Community' },
 ];

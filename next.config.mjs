a644f295088699043ae718798adb9f1e14d412c2
jedinrk <jedi.nrk@gmail.com>
2024-06-20 20:18:10 +0400

    set image optimisation to false

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   output: 'export',
   distDir: '_static',
   images: {
+    unoptimized: true, // Set to false for optimization
     remotePatterns: [
       {
         //https://cdn.prod.website-files.com/6365d860c7b7a7191055eb8a/66404ae94fe39eba6eb74858_AlphaWave.svg

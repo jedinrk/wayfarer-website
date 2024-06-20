e828851d68a4fe895ba2ae0f1eebee6f7e474fc4
jedinrk <jedi.nrk@gmail.com>
2024-06-20 14:11:58 +0400

    Updated NavBar styling

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -29,8 +29,8 @@ function Navbar() {
   const logoSrc = useResponsiveImage(HomeLogoLarge, HomeLogoSmall);
 
   return (
-    <div className="absolute z-10 w-full p-4">
-      <nav className="flex justify-start items-center w-full max-w-340 m-auto">
+    <div className="absolute z-10 w-full px-8 py-4">
+      <nav className="flex justify-between items-center w-full m-auto">
         {/* Logo */}
         <Link href="/" className="md:w-[334px] md:h-[58px]">
           <Image

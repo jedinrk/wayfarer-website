0b92bef56be6fdbabe58e59e485dd01e228105c4
jedinrk <jedi.nrk@gmail.com>
2024-06-20 14:12:11 +0400

    Fixed Banner Background color

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import Image from "next/image";
 import Button from "@/components/Button";
 import BannerImage from '../public/banner-image.png'
+import BannerRightBg from "../public/banner-right-bg.png"
 
 function Banner() {
   return (
-    <section className="w-full md:h-[50vw] lg:h-[49vw] xl:h-[49vw] xl:max-h-[55rem] pt-[90px] pb-[74.66vw] md:pb-0 bg-theme-200 relative">
+    <section className="w-full md:h-[50vw] lg:h-[49vw] xl:h-[49vw] xl:max-h-[55rem] pt-[90px] pb-[74.66vw] md:pb-0 bg-gradient-to-r from-theme-200 to-theme-1100 relative">
       <div className="relative z-10 w-full max-w-320 md:h-full p-4 md:p-5 md:pb-8 m-auto flex banner-inner">
         <div className="flex flex-col justify-center">
           <span className="block">TRANSPORTATION SOLUTIONS</span>

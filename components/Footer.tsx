344dfe09a22f67a520f052d19f538a0b123e0732
jedinrk <jedi.nrk@gmail.com>
2024-06-21 05:57:10 +0400

    Fixed Footer responsiveness

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,18 +5,26 @@ import Instagram from "../public/instagram.svg";
 import X from "../public/x.svg";
 import Linkedin from "../public/linkedIn.svg";
 import Youtube from "../public/youtube.svg";
+import SmallTruck from "../public/small-truck.png";
 
 function Footer() {
   return (
     <footer>
       <div className="w-full h-px bg-stone-200" />
-      <div className="w-full max-w-320 m-auto px-30 py-14 flex items-center justify-between">
+      <div className="w-full max-w-320 m-auto px-30 py-14 flex flex-col md:flex-row md:items-center md:justify-between relative">
         {/* Logo */}
-        <Image src={Logo} alt="Logo" />
+        <Image className="mx-4 md:mx-0" src={Logo} alt="Logo" />
+        <Image
+          className="absolute top-1/2 left-1/2 transform -translate-y-1/2 md:hidden"
+          src={SmallTruck}
+          alt=""
+          width={150}
+          height={150}
+        />
 
-        <div className="flex justify-between gap-12">
+        <div className="flex flex-col mt-14 mx-4 gap-4 md:mx-0 md:flex-row md:justify-between md:gap-12">
           {/* Navigation Links */}
-          <nav className="flex gap-12">
+          <nav className="flex flex-col gap-4 md:flex-row md:gap-12">
             <a
               href="#mission"
               className="text-gray-800 text-sm font-bold uppercase tracking-tight"
@@ -86,25 +94,29 @@ function Footer() {
 
       <div className="w-full h-px bg-stone-200" />
 
-      <div className="w-full max-w-320 m-auto pt-8 pb-24 flex items-center justify-center gap-6">
-        <div className="text-slate-500 text-sm font-normal leading-normal opacity-45">
+      <div className="w-full max-w-320 m-auto pt-8 pb-24 flex flex-col justify-start gap-2 md:flex-row md:items-center md:justify-center md:gap-6">
+        <div className="text-slate-500 text-sm font-normal leading-normal opacity-45 mx-4 md:mx-0">
           2024 Wayfarer. All rights reserved.
         </div>
-        <div className="flex gap-3 items-center">
+        <div className="flex flex-col gap-2 mx-4 md:mx-0 md:flex-row md:gap-3 md:items-center">
           <a
             href="/privacy-policy"
             className="text-slate-500 text-sm font-normal underline leading-normal opacity-45"
           >
             Privacy Policy
           </a>
-          <span className="text-slate-500 text-2xl font-normal leading-normal opacity-45">•</span>
+          <span className="text-slate-500 text-2xl font-normal leading-normal opacity-45 hidden md:block">
+            •
+          </span>
           <a
             href="/terms-of-service"
             className="text-slate-500 text-sm font-normal underline leading-normal opacity-45"
           >
             Terms of Service
           </a>
-          <span className="text-slate-500 text-2xl font-normal leading-normal opacity-45">•</span>
+          <span className="text-slate-500 text-2xl font-normal leading-normal opacity-45 hidden md:block">
+            •
+          </span>
           <a
             href="/cookies-settings"
             className="text-slate-500 text-sm font-normal underline leading-normal opacity-45"

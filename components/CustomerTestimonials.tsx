f0051b75802ae15ab846caaa86f229c253d24a0e
jedinrk <jedi.nrk@gmail.com>
2024-06-21 04:53:49 +0400

    Fixed Customer Testimonial responsiveness

--- components/CustomerTestimonials.tsx
+++ components/CustomerTestimonials.tsx
@@ -1,19 +1,42 @@
 import Testimonial, { Testimony } from "./Testimonial";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 const CustomerTestimonials: React.FC<{
   testimonies: Testimony[];
 }> = ({ testimonies }) => {
   return (
-    <section id="testimonials" className="w-full max-w-320 m-auto px-4 md:px-5 py-14 md:py-20 xl:py-28 flex flex-col">
+    <section
+      id="testimonials"
+      className="w-full max-w-320 m-auto px-4 md:px-5 py-14 md:py-20 xl:py-28 flex flex-col"
+    >
       <div className="w-full max-w-320 m-auto">
-        <h2 className="uppercase text-theme-text-700 h6 mb-[.5em]">Customer testimonials</h2>
+        <h2 className="uppercase text-theme-text-700 h6 mb-[.5em]">
+          Customer testimonials
+        </h2>
         <h3 className="h2">
-          Client Stories: <br />Our Impact in Action
+          Client Stories: <br />
+          Our Impact in Action
         </h3>
-        <div className="mt-8 md:mt-12 xl:mt-20 flex justify-between gap-[64px]">
-          {testimonies.map((testimony, index) => (
-            <Testimonial key={index} testimony={testimony} />
-          ))}
+        <div className="mt-8 md:mt-12 xl:mt-20">
+          <Swiper
+            spaceBetween={64}
+            slidesPerView={1}
+            pagination={{ clickable: true }}
+            navigation
+            breakpoints={{
+              768: {
+                slidesPerView: 2,
+                spaceBetween: 64,
+              }
+            }}
+          >
+            {testimonies.map((testimony, index) => (
+              <SwiperSlide key={index}>
+                <Testimonial testimony={testimony} />
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
       </div>
     </section>

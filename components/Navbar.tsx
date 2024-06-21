69329a3213e9af2c9d8c980fc7669c4fc77cc7a4
jedinrk <jedi.nrk@gmail.com>
2024-06-21 07:39:07 +0400

    added hamburger menu to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,30 +6,23 @@ import Link from "next/link";
 import HomeLogoLarge from "../public/home_logo_large.svg";
 import HomeLogoSmall from "../public/home_logo_small.svg";
 import MenuIcon from "../public/menu-icon.svg";
+import CloseIcon from "../public/ic_close.svg";
 import { useEffect, useState } from "react";
 import Button from "./Button";
 
-function useResponsiveImage(largeSrc: any, smallSrc: any, breakpoint = 768) {
-  const [src, setSrc] = useState(largeSrc);
-
-  useEffect(() => {
-    const updateSrc = () => {
-      setSrc(window.innerWidth <= breakpoint ? smallSrc : largeSrc);
-    };
-    updateSrc();
-    window.addEventListener("resize", updateSrc);
-
-    return () => window.removeEventListener("resize", updateSrc);
-  }, [largeSrc, smallSrc, breakpoint]);
-
-  return src;
-}
-
 function Navbar() {
-  const logoSrc = useResponsiveImage(HomeLogoLarge, HomeLogoSmall);
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  function handleMenuClick(event: any): void {
+    setMenuOpen(true);
+  }
+
+  function handleMenuClose(event: any): void {
+    setMenuOpen(false);
+  }
 
   return (
-    <div className="absolute z-10 w-full px-12 py-8">
+    <div className="absolute z-30 w-full px-12 py-8">
       <nav className="flex justify-between items-center w-full m-auto">
         {/* Logo */}
         <Link href="/" className="hidden md:block md:w-[334px] md:h-[58px]">
@@ -51,11 +44,7 @@ function Navbar() {
           {/* Menu Items */}
           <ul className="lg:flex hidden flex-1 align-middle justify-between max-w-[34rem] ml-16 mr-8 2xl:m-0 2xl:max-w-[unset] 2xl:gap-12 2xl:justify-center">
             {NAV_LINKS.map((link) => (
-              <Link
-                href={link.href}
-                key={link.key}
-                className=""
-              >
+              <Link href={link.href} key={link.key}>
                 {link.label}
               </Link>
             ))}
@@ -63,20 +52,34 @@ function Navbar() {
 
           {/* Let's talk Button */}
           <div className="hidden lg:block">
-            <Button
-              type={"submit"}
-              title={"Let's Talk"}
-              variant={""} />
+            <Button type={"submit"} title={"Let's Talk"} variant={""} />
           </div>
         </div>
-        <button className="lg:hidden p-2 ml-4">
-          <Image
-            src={MenuIcon}
-            alt="MenuIcon"
-            width={32}
-            height={32}
-          />
+        <button className="lg:hidden" onClick={handleMenuClick}>
+          <Image src={MenuIcon} alt="MenuIcon" width={32} height={32} />
         </button>
+        <div
+          className={
+            isMenuOpen
+              ? "fixed right-0 top-0 w-[65%] sm:hidden h-screen p-10 ease-in duration-300 bg-lime-700/90"
+              : "fixed -right-full h-screen sm:hidden top-0 p-10 ease-in duration-500"
+          }
+        >
+          <div className="w-full flex justify-end">
+            <button onClick={handleMenuClose}>
+              <Image src={CloseIcon} alt={""} width={32} height={32} />
+            </button>
+          </div>
+
+          {/* Menu Items */}
+          <ul className="flex flex-col py-10 gap-4">
+            {NAV_LINKS.map((link) => (
+              <Link href={link.href} key={link.key} className="text-white/80 hover:text-white">
+                {link.label}
+              </Link>
+            ))}
+          </ul>
+        </div>
       </nav>
     </div>
   );

224228fac31d4478c5952cec5e5f11f2b5599a5c
jedinrk <jedi.nrk@gmail.com>
2024-09-08 20:26:39 +0530

    Updating ContactUs Content sections

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -4,14 +4,11 @@ import Image from "next/image";
 import IconEmail from "../public/ic_email.svg";
 import IconPhone from "../public/ic_phone.svg";
 import IconPin from "../public/ic_pin.svg";
-import ContactUsTruck from "../public/contact-us-truck.png";
+import ContactUsBg from "../public/contact-left-bg.png";
 import CustomTextField from "./CustomTextField";
 import CustomPhoneInput from "./CustomPhoneInput";
 import InterestCheckbox from "./InterestCheckbox";
 import Button from "./Button";
-
-import ContactUsBgLogo from "../public/contact-us-bg-logo.png";
-//import ContactUsBgLogo from "../components/ContactUsBgLogo";
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
@@ -100,11 +97,13 @@ const FormComponent = () => {
           />
         </div>
       </div>
-      <InterestCheckbox
-        name="interestedIn"
-        value={formData.interestedIn}
-        onChange={handleChange}
-      />
+      <div className="form-group flex flex-col">
+        <InterestCheckbox
+          name="interestedIn"
+          value={formData.interestedIn}
+          onChange={handleChange}
+        />
+      </div>
       <div className="form-group flex flex-col">
         <CustomTextField
           fullWidth
@@ -140,60 +139,80 @@ function ContactUs() {
   return (
     <section id="contact-us" className="w-full">
       <div className="w-full max-w-320 m-auto px-4 md:px-5 py-14 md:py-20 xl:py-28">
-        <div className="md:flex md:flex-col lg:flex-row items-center gap-[134px] max-w-fit">
-          <div className="relative lg:flex-1 rounded-3xl bg-[#E8F4CD]">
+        <div className="md:flex md:flex-col lg:flex-row items-center gap-16">
+          <div className="rounded-3xl overflow-hidden relative">
             <Image
-              src={ContactUsBgLogo}
-              alt="Contact Us Background Logo"
-              className="absolute inset-0 object-cover z-10 mt-2"
+              src={ContactUsBg}
+              alt="Contact Us Background"
+              className="h-full w-full object-cover z-10"
             />
-            {/* <div className="absolute inset-0 object-cover z-10">
-              <ContactUsBgLogo />
-            </div> */}
-            <div className="relative z-20">
-              <div className="p-4 md:p-7 lg:p-9 pt-[47px]">
-                <h2 className="uppercase mb-[.5em] text-theme-text-700 h6">
-                  Contact Us
-                </h2>
-                <h3 className="h2 mb-6 font-medium leading-[1.07]">
-                  Send a message <br /> to our team
-                </h3>
-                <div className="flex flex-col gap-4">
-                  <div className="flex gap-4">
-                    <Image src={IconEmail} alt="IconEmail" />
-                    <a
-                      className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
-                      href="mailto:wayfarerlogisticspvtltd@gmail.com"
-                    >
-                      wayfarerlogisticspvtltd@gmail.com
-                    </a>
-                  </div>
-                  <div className="flex gap-4">
-                    <Image src={IconPhone} alt="IconPhone" />
-                    <a
-                      className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
-                      href="tel:+919746710770"
-                    >
-                      +91 9746710770
-                    </a>
-                  </div>
-                  <div className="flex gap-4 items-start">
-                    <Image src={IconPin} alt="IconPin" className="pt-2" />
-                    <address className="not-italic opacity-80 text-[#1a282b] text-lg font-medium leading-[1.3]">
-                      Ground Floor, 14/153,
-                      <br />
-                      Vazhakkoottathil Building, Azheekkal,
-                      <br />
-                      Vypin, Kerala, 682508
-                    </address>
+            <div className="flex flex-col p-4 md:p-7 lg:p-9 pt-[47px] z-20 absolute top-0 left-0 right-0 bottom-0">
+              <h2 className="uppercase mb-[.5em] text-theme-text-700 h6">
+                Contact Us
+              </h2>
+              <h3 className="h2 mb-6 font-medium leading-[1.07]">
+                Send a message <br /> to our team
+              </h3>
+              <div className="flex flex-col gap-4">
+                <div className="flex gap-4 items-start">
+                  <Image src={IconPin} alt="IconPin" className="pt-2" />
+                  <address className="not-italic opacity-80 text-[#1a282b] text-lg font-medium leading-[1.3]">
+                    14/153, Ground Floor
+                    <br />
+                    Vazhakkoottathil Building, Panikkarupady
+                    <br />
+                    Near Vallarpadam International Port
+                    <br />
+                    Ernakulam, Kerala, 682508
+                  </address>
+                </div>
+                <div className="flex justify-start">
+                  <Image
+                    src={IconPhone}
+                    alt="IconPhone"
+                    className="mr-4 pt-2"
+                  />
+                  <div className="flex flex-col">
+                    <div className="flex flex-row">
+                      <a
+                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose mr-1"
+                        href="tel:+919746710770"
+                      >
+                        +91 9746710770,
+                      </a>
+                      <a
+                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
+                        href="tel:+919567026314"
+                      >
+                        +91 9567026314
+                      </a>
+                    </div>
+                    <div className="flex flex-row">
+                      <a
+                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose mr-1"
+                        href="tel:+919746698889"
+                      >
+                        +91 9746698889,
+                      </a>
+                      <a
+                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
+                        href="tel:+919746021116"
+                      >
+                        +91 9746021116
+                      </a>
+                    </div>
                   </div>
                 </div>
+                <div className="flex gap-4">
+                  <Image src={IconEmail} alt="IconEmail" />
+                  <a
+                    className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
+                    href="mailto:wayfarerlogisticspvtltd@gmail.com"
+                  >
+                    wayfarerlogisticspvtltd@gmail.com
+                  </a>
+                </div>
               </div>
-              <Image
-                src={ContactUsTruck}
-                alt=""
-                className="mt-0 md:mt-[50px]"
-              />
             </div>
           </div>
           <div className="lg:flex-1 mt-12 md:mt-16 lg:mt-0">

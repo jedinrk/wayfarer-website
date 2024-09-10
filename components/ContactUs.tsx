2c08a1eafe351a4b34d56d1477d97e80a4a8923e
jedinrk <jedi.nrk@gmail.com>
2024-09-10 08:30:53 +0530

    Moved ContactUs Left pane as a component

--- components/ContactUs.tsx
+++ components/ContactUs.tsx
@@ -9,9 +9,89 @@ import CustomTextField from "./CustomTextField";
 import CustomPhoneInput from "./CustomPhoneInput";
 import InterestCheckbox from "./InterestCheckbox";
 import Button from "./Button";
+import ArrowRight from "../public/arrow-right.svg";
 
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm, ValidationError } from "@formspree/react";
 
+const SubmittedComponent = () => {
+  return <></>;
+};
+
+const ContactUsLeftPane = () => {
+  return (
+    <div className="rounded-3xl overflow-hidden relative">
+      <Image
+        src={ContactUsBg}
+        alt="Contact Us Background"
+        className="h-full w-full object-cover z-10"
+      />
+      <div className="flex flex-col p-4 md:p-7 lg:p-9 pt-[47px] z-20 absolute top-0 left-0 right-0 bottom-0">
+        <h2 className="uppercase mb-[.5em] text-theme-text-700 h6">
+          Contact Us
+        </h2>
+        <h3 className="h2 mb-6 font-medium leading-[1.07]">
+          Send a message <br /> to our team
+        </h3>
+        <div className="flex flex-col gap-4">
+          <div className="flex gap-4 items-start">
+            <Image src={IconPin} alt="IconPin" className="pt-2" />
+            <address className="not-italic opacity-80 text-[#1a282b] text-lg font-medium leading-[1.3]">
+              14/153, Ground Floor
+              <br />
+              Vazhakkoottathil Building, Panikkarupady
+              <br />
+              Near Vallarpadam International Port
+              <br />
+              Ernakulam, Kerala, 682508
+            </address>
+          </div>
+          <div className="flex justify-start">
+            <Image src={IconPhone} alt="IconPhone" className="mr-4 pt-2" />
+            <div className="flex flex-col">
+              <div className="flex flex-row">
+                <a
+                  className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose mr-1"
+                  href="tel:+919746710770"
+                >
+                  +91 9746710770,
+                </a>
+                <a
+                  className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
+                  href="tel:+919567026314"
+                >
+                  +91 9567026314
+                </a>
+              </div>
+              <div className="flex flex-row">
+                <a
+                  className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose mr-1"
+                  href="tel:+919746698889"
+                >
+                  +91 9746698889,
+                </a>
+                <a
+                  className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
+                  href="tel:+919746021116"
+                >
+                  +91 9746021116
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex gap-4">
+            <Image src={IconEmail} alt="IconEmail" />
+            <a
+              className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
+              href="mailto:wayfarerlogisticspvtltd@gmail.com"
+            >
+              wayfarerlogisticspvtltd@gmail.com
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 const FormComponent = () => {
   const [formData, setFormData] = useState({
@@ -20,7 +100,6 @@ const FormComponent = () => {
     email: "",
     countryCode: "",
     phoneNumber: "",
-    company: "",
     interestedIn: "",
     message: "",
   });
@@ -29,20 +108,15 @@ const FormComponent = () => {
   const [state, handleSubmit] = useForm("xanwkkjr");
 
   if (state.succeeded) {
-      return <p>Thanks for joining!</p>;
+    console.log("state: ", state);
+    return <p>Thanks for joining!</p>;
+  } else {
+    console.log("state: ", state);
   }
 
   const handleChange = (event: { target: { name: any; value: any } }) => {
-    console.log(event.target.name);
-    console.log(event.target.value);
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
-
-  // const handleSubmit = (event: { preventDefault: () => void }) => {
-  //   event.preventDefault();
-  //   console.log("Form submitted!", formData);
-  //   // Handle form submission logic here (e.g., send data to server)
-  // };
 
   const reCaptchaOnChange = (value: any) => {
     console.log("Captcha value:", value);
@@ -131,13 +205,25 @@ const FormComponent = () => {
           sitekey="6LfHYf0pAAAAAM0VVdPSwITD6H98UHJrXh0L0Kln"
           onChange={reCaptchaOnChange}
         />
-        <Button
-          title="Submit"
-          disabled={!submitEnabled}
-          icon
-          width="w-[100px]"
-          height="h-[32px]"
-        />
+        <button
+          className="w-fit h-12 px-7 py-2 bg-[#4b8200] rounded-[44px] justify-center items-center gap-1 inline-flex"
+          type="submit"
+        >
+          <div className="p-1 justify-center items-center gap-2 flex">
+            <span className="text-white text-lg font-semibold font-['Figtree'] leading-normal tracking-wide">
+              Submit
+            </span>
+          </div>
+          <div className="w-6 h-6 relative">
+            <Image
+              className="default-icon"
+              src={ArrowRight}
+              alt="ArrowRight"
+              width="28"
+              height="28"
+            />
+          </div>
+        </button>
       </div>
     </form>
   );
@@ -148,81 +234,7 @@ function ContactUs() {
     <section id="contact-us" className="w-full">
       <div className="w-full max-w-320 m-auto px-4 md:px-5 py-14 md:py-20 xl:py-28">
         <div className="md:flex md:flex-col lg:flex-row items-center gap-16">
-          <div className="rounded-3xl overflow-hidden relative">
-            <Image
-              src={ContactUsBg}
-              alt="Contact Us Background"
-              className="h-full w-full object-cover z-10"
-            />
-            <div className="flex flex-col p-4 md:p-7 lg:p-9 pt-[47px] z-20 absolute top-0 left-0 right-0 bottom-0">
-              <h2 className="uppercase mb-[.5em] text-theme-text-700 h6">
-                Contact Us
-              </h2>
-              <h3 className="h2 mb-6 font-medium leading-[1.07]">
-                Send a message <br /> to our team
-              </h3>
-              <div className="flex flex-col gap-4">
-                <div className="flex gap-4 items-start">
-                  <Image src={IconPin} alt="IconPin" className="pt-2" />
-                  <address className="not-italic opacity-80 text-[#1a282b] text-lg font-medium leading-[1.3]">
-                    14/153, Ground Floor
-                    <br />
-                    Vazhakkoottathil Building, Panikkarupady
-                    <br />
-                    Near Vallarpadam International Port
-                    <br />
-                    Ernakulam, Kerala, 682508
-                  </address>
-                </div>
-                <div className="flex justify-start">
-                  <Image
-                    src={IconPhone}
-                    alt="IconPhone"
-                    className="mr-4 pt-2"
-                  />
-                  <div className="flex flex-col">
-                    <div className="flex flex-row">
-                      <a
-                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose mr-1"
-                        href="tel:+919746710770"
-                      >
-                        +91 9746710770,
-                      </a>
-                      <a
-                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
-                        href="tel:+919567026314"
-                      >
-                        +91 9567026314
-                      </a>
-                    </div>
-                    <div className="flex flex-row">
-                      <a
-                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose mr-1"
-                        href="tel:+919746698889"
-                      >
-                        +91 9746698889,
-                      </a>
-                      <a
-                        className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
-                        href="tel:+919746021116"
-                      >
-                        +91 9746021116
-                      </a>
-                    </div>
-                  </div>
-                </div>
-                <div className="flex gap-4">
-                  <Image src={IconEmail} alt="IconEmail" />
-                  <a
-                    className="opacity-80 text-[#1a282b] text-lg font-medium leading-loose"
-                    href="mailto:wayfarerlogisticspvtltd@gmail.com"
-                  >
-                    wayfarerlogisticspvtltd@gmail.com
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
+          <ContactUsLeftPane />
           <div className="lg:flex-1 mt-12 md:mt-16 lg:mt-0">
             <FormComponent />
           </div>

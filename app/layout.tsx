5955a61723e480ab16375fe8e28e6b9976668247
Jinson <web2feel@gmail.com>
2025-01-11 00:19:59 +0530

    figtree font loading refactored with next/font/google

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,11 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-
+import { Figtree } from 'next/font/google'
+ 
+const figtree = Figtree({
+  subsets: ['latin'],
+})
 export const metadata: Metadata = {
   title: "Wayfarer Logistics",
   description: "Sustainable Logistics for a Greener Future",
@@ -14,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="scroll-smooth focus:scroll-auto">
+    <html lang="en" className={`scroll-smooth focus:scroll-auto ${figtree.className}`}>
       <body className="relative">
         <Navbar />
         <main className="relative overflow-hidden">{children}</main>
